2490ce2039876cb3adb60b46978dcbd529921253
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-16 21:43:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   speed = 60
-  speed < 40 ? false : true
+  speed < 40 ? false : false
 end
 
 
 def not_safe?(speed)
 speed = 60
-speed < 40 ? false : true
+speed < 40 ? false : false
 end
