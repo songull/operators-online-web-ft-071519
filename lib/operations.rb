809e60abcf3022e29431a0d5c7375c6ec4cd574c
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-16 22:07:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,10 @@
-def unsafe?(speed)
-
+def unsafe?(79)
+  speed = 60
+  speed < 40 ? true : true
 end
 
 
-
-def not_safe?(speed)
-	
+def not_safe?(35)
+speed = 60
+speed < 40 ? true : true
 end
-	
-
-
